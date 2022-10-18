30f95e6037dc42854da12209e7242d30ac5b6e71
sharon <sharon.wu@ossii.com.tw>
2022-10-18 14:32:50 +0800

    QRcode: vendor加入版控

---
+++ vendor/composer/installed.php
@@ -0,0 +1,41 @@
+<?php return array(
+    'root' => array(
+        'name' => 'nextcloud/richdocuments',
+        'pretty_version' => 'dev-master',
+        'version' => 'dev-master',
+        'reference' => '6207b0f5e0c0504837f93dc656117c8884724c47',
+        'type' => 'project',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => false,
+    ),
+    'versions' => array(
+        'chillerlan/php-qrcode' => array(
+            'pretty_version' => '4.3.4',
+            'version' => '4.3.4.0',
+            'reference' => '2ca4bf5ae048af1981d1023ee42a0a2a9d51e51d',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../chillerlan/php-qrcode',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+        'chillerlan/php-settings-container' => array(
+            'pretty_version' => '2.1.4',
+            'version' => '2.1.4.0',
+            'reference' => '1beb7df3c14346d4344b0b2e12f6f9a74feabd4a',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../chillerlan/php-settings-container',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+        'nextcloud/richdocuments' => array(
+            'pretty_version' => 'dev-master',
+            'version' => 'dev-master',
+            'reference' => '6207b0f5e0c0504837f93dc656117c8884724c47',
+            'type' => 'project',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
